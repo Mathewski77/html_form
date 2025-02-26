bd572b9bca5c285d4eed79bf8b978909dd0bd8cc
Mathewski77 <mlacsina23-0408@cca.edu.ph>
2025-02-26 19:51:08 +0800

    sumary

--- action_page.php
+++ action_page.php
@@ -1,9 +1,12 @@
 <?php
+    echo "<pre>";
     var_dump($_POST);
+    echo "</pre>";
+    echo "<br><br>";
 
-    $fullname='';
-    $gender='';
-    $country='';
+    $fullname = $_POST['FullName'];
+    $gender = $_POST['Gender'];
+    $country = $_POST['Country'];
 
 ?>
 
--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     </head>
     <body style="background-color:skyblue">
         <h1> ADD STUDENT </h1>
-        <form action = "action_page.php" method="GET">
+        <form action = "action_page.php" method="POST">
             <table border="1" cellspacing="5" width="25%">
                 <tr>
                     <th>Question</th>
@@ -17,16 +17,16 @@
                 <tr>
                     <td>Gender</td>
                     <td> 
-                        <input type="radio" id="male" name="value" value="male" >
+                        <input type="radio" id="male" name="Gender" value="male" >
                         <label for="male">Male</label>
-                        <input type = "radio" id="female" name="value" value="female">
+                        <input type = "radio" id="female" name="Gender" value="female">
                         <label for="female">Female</label>
                     </td>
                 </tr>
                 <tr>
                     <td>Country</td>
                     <td>
-                        <select name="country">
+                        <select name="Country">
                             <option value="PH">-SELECT YOUR COUNTRY-</option>
                             <option value="PH">Philippines</option>
                             <option value="AUS">Australia</option>
